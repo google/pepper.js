9cc640d44f93ebd07b9c93cda2c964938c43dc46
Nick Bray <ncbray@chromium.org>
2013-03-04 14:41:45 -0800

    Refactor build.

--- build.sh
+++ build.sh
@@ -9,31 +9,28 @@ set -e
 # Make sure the main output directory exists.
 mkdir -p out
 
-FLAGS="-std=c++11 -O2 --closure 1 --minify 0 --js-library library_ppapi.js --pre-js ppapi.js"
-IN_DIR=examples/hello_world
-OUT_DIR=out/hello_world
-
-# Make sure the output directory exists.
-mkdir -p $OUT_DIR
-
-# Stage data requried to run in the browser.
-cp examples/common.js $IN_DIR/*.html $IN_DIR/*.js $OUT_DIR
-
-# Compile the hello world example.
-$EMCC $FLAGS -I$NACL_SDK_ROOT/include ppapi_stub.cc $IN_DIR/hello_world.cc -o $OUT_DIR/hello_world.js -s EXPORTED_FUNCTIONS="['_Startup']"
+FLAGS="-std=c++11 -O0 --closure 0 --minify 0 --js-library library_ppapi.js --pre-js ppapi.js -I$NACL_SDK_ROOT/include"
 
 # Files that are normally compiled into libppapi_cpp.
 D=$NACL_SDK_ROOT/src/ppapi_cpp
 PPAPI_CPP="$D/ppp_entrypoints.cc $D/module.cc $D/instance.cc $D/instance_handle.cc $D/var.cc $D/url_request_info.cc $D/url_response_info.cc $D/url_loader.cc $D/resource.cc $D/lock.cc"
 
-IN_DIR=examples/geturl
-OUT_DIR=out/geturl
+example() {
+  NAME=$1
+  IN_DIR=examples/$NAME
+  OUT_DIR=out/$NAME
 
-# Make sure the output directory exists.
-mkdir -p $OUT_DIR
+  # Make sure the output directory exists.
+  mkdir -p $OUT_DIR
 
-# Stage data requried to run in the browser.
-cp examples/common.js $IN_DIR/*.html $IN_DIR/*.js $OUT_DIR
+  # Stage data requried to run in the browser.
+  cp examples/common.js $IN_DIR/*.html $IN_DIR/*.js $OUT_DIR
+}
 
-# Compile the geturl example.
-$EMCC $FLAGS -I$NACL_SDK_ROOT/include ppapi_stub.cc $PPAPI_CPP $IN_DIR/geturl.cc $IN_DIR/geturl_handler.cc -o $OUT_DIR/geturl.js -s EXPORTED_FUNCTIONS="['_Startup']"
+example "hello_world"
+SOURCES="$IN_DIR/hello_world.cc"
+$EMCC $FLAGS ppapi_stub.cc $SOURCES -o $OUT_DIR/$NAME.js -s EXPORTED_FUNCTIONS="['_Startup']"
+
+example "geturl"
+SOURCES="$IN_DIR/geturl.cc $IN_DIR/geturl_handler.cc"
+$EMCC $FLAGS $PPAPI_CPP ppapi_stub.cc $SOURCES -o $OUT_DIR/$NAME.js -s EXPORTED_FUNCTIONS="['_Startup']"
