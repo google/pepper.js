dbe0a93ed702a08992592b6bd50215e2750a771d
Nick Bray <ncbray@chromium.org>
2013-09-17 18:19:42 -0700

    Add LICENSE.

--- ppapi_preamble.js
+++ ppapi_preamble.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // TODO(ncbray): re-enable once Emscripten stops including code with octal values.
 //"use strict";
 
--- wrappers/testing.js
+++ wrappers/testing.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 (function() {
 
   var Testing_Dev_ReadImageData = function(device_context_2d, image, top_left) {
