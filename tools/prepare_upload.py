2c66f2331906806adb89150de10a16ac1a2516a6
Nick Bray <ncbray@chromium.org>
2013-10-09 14:08:15 -0700

    Improve example CSS styling.

--- tools/prepare_upload.py
+++ tools/prepare_upload.py
@@ -5,25 +5,26 @@ import re
 import posixpath
 import subprocess
 
+# Unfortunately the build products are scattered throughout the examples
+# directory, so filter by type
+filetype = re.compile("\.(js|html|css|nmf|nexe|pexe|jpg|png|raw|frag|vert|js\.map)$")
 
-filetype = re.compile("\.(js|html|css|nmf|nexe|pexe|jpg|png|frag|vert|js\.map)$")
-filetype = re.compile("\.(frag|vert|raw)$")
-
+os.chdir("examples")
 
 def build(toolchain, mode):
   subprocess.check_call(["make", "CONFIG=" + mode, "TOOLCHAIN=" + toolchain])
 
-os.chdir("examples")
-
+# Make sure everything is built.
 for toolchain in ["newlib", "pnacl", "emscripten"]:
   for mode in ["Debug", "Release"]:
-    pass#build(toolchain, mode)
+    build(toolchain, mode)
 
 def upload(fn):
   cmd = ["gsutil", "cp", "-z", "html,js,css,nmf,nexe,pexe,map", "-a", "public-read", fn,  "gs://" + posixpath.normpath(posixpath.join("gonacl/pepper.js", fn))]
   print " ".join(cmd)
   subprocess.check_call(cmd)
 
+# Upload.
 for path, dirs, files in os.walk("."):
   path = os.path.relpath(path, ".")
   for f in files:
